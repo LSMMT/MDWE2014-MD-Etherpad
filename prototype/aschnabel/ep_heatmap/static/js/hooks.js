13d77b2d3b4702f6172f2455124706cd8bd23780
Armin Schnabel <armin.schnabel@gmail.com>
2014-12-10 18:39:12 +0100

    init process successfully fixed

--- prototype/aschnabel/ep_heatmap/static/js/hooks.js
+++ prototype/aschnabel/ep_heatmap/static/js/hooks.js
@@ -4,12 +4,30 @@
  *
  */
 
-var debug = true, initHM = false, initDelay=0, lineNumber=-1, initChangeset = new Array();
+var debug = false, initHM = true, initDelay=0, lineNumber=-1, initChangeset = new Array();
 var ep_activity = new Array();
 
 
 // Gets called every second
 exports.aceEditEvent = function(hook_name, args, cb) {
+
+  // Pre-init process: save changeset for init process
+  if (initHM) initChangeset = args.rep.alines;
+
+  // Trace Changes after init process
+  else if ((args.callstack.docTextChanged == true || args.callstack.type == "applyChangesToBase")) {
+
+      var changedLine = findChangedLine(args.rep.alines);
+      ep_activity[changedLine][0] = args.rep.alines[changedLine]; // save new changeset
+      ep_activity[changedLine][1]++; // increment activity
+      console.table(ep_activity);
+
+      //console.log("aceEditEvent-rep-OBJECT: "+JSON.stringify(args.rep))
+      //console.log("aceEditEvent-documentAttributeManager-OBJECT: "+JSON.stringify(args.documentAttributeManager ))
+      //console.log("aceEditEvent-callstack -OBJECT: "+JSON.stringify(args.callstack  ))
+      //ERROR:createChangeset(args.rep.alines[changedLine]);
+  }
+
   // Decay of activity
   for(var i=0; i<ep_activity.length; i++) {
     if (ep_activity[i][1]>0) {
@@ -19,36 +37,17 @@ exports.aceEditEvent = function(hook_name, args, cb) {
     }
   }
 
-  // Gets called when changeset received
-  if ((args.callstack.docTextChanged == true || args.callstack.type == "applyChangesToBase")) {
-    if (args.rep.selEnd) {
-      // Preinit EditEvent - used to determin first change after init
-      if (!(initDelay<0)) initChangeset = args.rep.alines;
-      else {
-        // Trace activity after init process
-        var changedLine = findChangedLine(args.rep.alines);
-        ep_activity[changedLine][0] = args.rep.alines[changedLine]; // save new changeset
-        ep_activity[changedLine][1]++; // increment activity
-        console.table(ep_activity);
-
-        //console.log("aceEditEvent-rep-OBJECT: "+JSON.stringify(args.rep))
-        //console.log("aceEditEvent-documentAttributeManager-OBJECT: "+JSON.stringify(args.documentAttributeManager ))
-        //console.log("aceEditEvent-callstack -OBJECT: "+JSON.stringify(args.callstack  ))
-        //ERROR:createChangeset(args.rep.alines[changedLine]);
-      }
-    }
-  }
   return cb();
-};
+}
+
 
 // Helper function to find the changed value between two arrays
 function findChangedLine(newChangesets) {
   console.log(newChangesets); // new changeset before changes
-  for(var i=0; i<newChangesets.length; i++) {
-    // Exception when ep_activity[i] doesnt exist yet (last line)
-    if (ep_activity[i] == undefined) { console.log("not found"); createLineArrayCS(i, "|1+1"); }
 
+  for(var i=0; i<newChangesets.length; i++) {
     if (newChangesets[i] !== ep_activity[i][0]) {
+
       // debugging / logging
       console.log("Changeline ("+i+"): "+ep_activity[i][0]+" --> "+newChangesets[i]);
       if (ep_activity[i-1] != undefined && newChangesets[i-1] != undefined) console.log("(preLine) "+ep_activity[i-1][0]+" --> "+newChangesets[i-1]);
@@ -64,10 +63,12 @@ function findChangedLine(newChangesets) {
   }
 }
 
+
 // Helper function: new line added (ENTER) -> update ep_activity
 function addedLine(position, chset) {
   ep_activity.splice(position, 0, new Array(chset, 0)); // splice(index, count_to_remove, addelement1, addelement2, ...)
 }
+
 
 // Helper function: line dropped (RETURN, buggy) -> update ep_activity
 function droppedLine(position) {
@@ -75,40 +76,39 @@ function droppedLine(position) {
 }
 
 
-// Gets called when DOM line is written -> for Init process
-exports.acePostWriteDomLineHTML = function(hook_name, args, cb) {
-  var lineContent = args.node.children[0].innerText,
-      lineId = args.node.id.substring(10);
-
-  // Create array containing padlines
-  if (initHM) {
-    lineNumber++;
-    console.log("init lineId ("+lineId+") lineNumber("+lineNumber+") lineContent("+(lineContent=="\n"?"\\n":lineContent)+")");
-    createLineArray(lineNumber);
-    initDelay--;
-    if (initDelay<1) { initHM=false; initDelay--; console.log("Init done: ep_activity[line][changeset, activity] \n"); console.table(ep_activity); }
-  }
-
-  // Count initDelay
-  else if (initDelay>=0) initDelay++;
-  return cb();
-};
-
-
-
 // Helper function for init array process
 function createLineArray(line) {
   var lineArray = new Array(initChangeset[line], 0); // lineArray[changeset, activity]
   ep_activity.push(lineArray);
-};
+}
 
 
-// Gets called when init process can start
+// Gets called when "pre-init" is done
 exports.postAceInit = function(hook_name, args, cb) {
-  //initDelay--;
   console.log("postAceInit-event");
-  console.log("start Init - initDelay("+initDelay+")");
-  initHM = true;
-  // generateHeatmap ( createNumericModel(ep_activity) )
+  initDelay = lineNumber;
+
+  // Init process: fill ep_activity
+  for (var i=0; i<initChangeset.length; i++) createLineArray(i);
+  console.log("Init done: ep_activity now:\n"); console.table(ep_activity);
+
+  initHM=false;
   return cb();
-};
+}
+
+
+// Gets called when DOM line is written -> pre init process
+exports.acePostWriteDomLineHTML = function(hook_name, args, cb) {
+  var lineContent = args.node.children[0].innerText,
+      lineId = args.node.id.substring(10);
+
+  // While init: pre init process
+  if (initHM) {
+    lineNumber++;
+    //if (debug) console.log("init lineId ("+lineId+") lineNumber("+lineNumber+") lineContent("+(lineContent=="\n"?"\\n":lineContent)+")");
+  }
+
+  // Count initDelay
+  else if (initDelay>0) initDelay--;
+  return cb();
+}
