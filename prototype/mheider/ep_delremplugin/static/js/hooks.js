0ceba4d73aee1f41490e0ad8806cc37ccf84d4d7
Markus Heider <markus.heider@bindoc.de>
2014-12-11 16:50:36 +0100

    added a etherpad plugin (prototype)
    this plugin tracks the text, which the user pastes or deletes in the
    current pad.

---
+++ prototype/mheider/ep_delremplugin/static/js/hooks.js
@@ -0,0 +1,18 @@
+exports.postAceInit = function(hook_name, args, cb) {
+
+    console.log("post ace init event!");
+
+    
+    return cb();
+};
+
+
+/*
+ * TODO: Documentation
+ */
+exports.acePostWriteDomLineHTML = function(hook_name, args, cb) {
+
+    console.log("acePostWriteDeomLineHTML");
+    
+    return cb();
+};
